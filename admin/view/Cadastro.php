793103a7f5c4e3e4b01be36d85024762d2d2a909
Marcelo Colla <colla.pr@gmail.com>
2019-03-29 21:17:40 -0300

    Alterando imagem para telas grandes

--- admin/view/Cadastro.php
+++ admin/view/Cadastro.php
@@ -18,7 +18,7 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/sb-admin-2.css" rel="stylesheet">
 </head>
 <body class="bg-gradient-primary">
   <div class="container">
@@ -30,10 +30,10 @@
           <div class="col-lg-7">
                 <img src="">
             <div class="p-5">
-                   
+
               <div class="text-center">
                     <img src="img/logo.jpg" class="img-fluid max-width: 10%" alt="Responsive image">
-                    
+
                 <h1 class="h4 text-gray-900 mb-4">Registre-se</h1>
               </div>
               <form class="user">
