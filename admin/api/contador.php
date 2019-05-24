bf06f74be4dd4c7c5f1cb64ff6d5b60216343bd1
Marcelo Colla <colla.pr@gmail.com>
2019-05-23 21:45:53 -0300

    Ajustando calculo de contador regressivo

---
+++ admin/api/contador.php
@@ -0,0 +1,9 @@
+<?php
+
+echo json_encode(array(
+    'success'=> true,
+    'id' => 123,
+    'data_inicio'=> '2019-05-23 20:00:50',
+    'data_final'=> '2019-05-24 23:30:00',
+    'placa' => 'ABC-1234'
+));
